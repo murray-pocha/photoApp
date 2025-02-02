9f81d0338442ecf96f4944f452d0e522f13ad72c
murray-pocha <murray.pocha@gmail.com>
2025-02-02 12:19:53 -0700

    make adjustment to follow BEM guidlines

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import "../styles/PhotoListItem.scss";
-import "../styles/PhotoListItem.scss";
+
 const PhotoListItem = ({ photo }) => {
- 
   return (
     <div className="photo-list__item">
-      <img src={photo.imageSource} alt={`Photo taken in ${photo.location.city}`} className="photo" />
-      <div className="photo-details">
-        <img src={photo.profile} alt={`${photo.username} profile`} className="profile-pic" />
-        <div>
+      <img 
+        src={photo.imageSource} 
+        alt={`Photo taken in ${photo.location.city}`} 
+        className="photo-list__image" 
+      />
+      <div className="photo-list__user-details">
+        <img 
+          src={photo.profile} 
+          alt={`${photo.username} profile`} 
+          className="photo-list__user-profile" 
+        />
+        <div className="photo-list__user-info">
           <h3>{photo.username}</h3>
-          <p>{photo.location.city}, {photo.location.country}</p>
+          <p className="photo-list__user-location">
+            {photo.location.city}, {photo.location.country}
+          </p>
         </div>
       </div>
     </div>
