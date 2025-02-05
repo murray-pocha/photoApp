78471905b23b88c1afc8c9862bc533f4e4cce3c1
murray-pocha <murray.pocha@gmail.com>
2025-02-05 11:09:03 -0700

    set default value for topics to make sure it is always an array, remove checks

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,28 +2,12 @@ import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+const TopicList = ({ topics = [] }) => {
+  
 
-const TopicList = () => {
   return (
     <ul className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topic) => (
+      {topics.map((topic) => (
         <TopicListItem key={topic.id} title={topic.title} />
       ))}
     </ul>
