16a08bd1ca5c2513f9ce8c7ef6aa49a98c63db39
murray-pocha <murray.pocha@gmail.com>
2025-02-05 11:08:22 -0700

    simplify how topics is passed to TopicList, remove fallback handling inside HomeRoute

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import TopNavigationBar from "./TopNavigationBar";
 import PhotoList from "./PhotoList";
+import TopicList from "./TopicList";
 import "../styles/HomeRoute.scss";
 
-const HomeRoute = ({ photos, isFavPhotoExist, onLikeToggle }) => {
+const HomeRoute = ({ photos, topics, isFavPhotoExist, onLikeToggle }) => {
+
   return (
     <div className="home-route">
       <TopNavigationBar isFavPhotoExist={isFavPhotoExist} />
+      <TopicList topics={topics || []} />
+
+      {Array.isArray(topics) && topics.length > 0 && (
+        <div className="topics-list">
+          {(topics || []).map((topic) => (
+            <button key={topic.id} className="topic-button">
+              {topic.title}
+            </button>
+          ))}
+        </div>
+      )}
       <PhotoList photos={photos} onLikeToggle={onLikeToggle} />
     </div>
   );
